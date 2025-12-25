b5414f0ee820b289239ddd0f89431e824666ee91
rockkult06 <okucukali@outlook.com>
2025-12-25 21:50:16 +0300

    h

--- components/ui/sidebar.tsx
+++ components/ui/sidebar.tsx
@@ -451,7 +451,7 @@ const SidebarGroupLabel = React.forwardRef<
         'group-data-[collapsible=icon]:-mt-8 group-data-[collapsible=icon]:opacity-0',
         className,
       )}
-      {...props}
+      {...(props as any)}
     />
   )
 })
@@ -474,7 +474,7 @@ const SidebarGroupAction = React.forwardRef<
         'group-data-[collapsible=icon]:hidden',
         className,
       )}
-      {...props}
+      {...(props as any)}
     />
   )
 })
@@ -571,7 +571,7 @@ const SidebarMenuButton = React.forwardRef<
         data-size={size}
         data-active={isActive}
         className={cn(sidebarMenuButtonVariants({ variant, size }), className)}
-        {...props}
+        {...(props as any)}
       />
     )
 
@@ -625,7 +625,7 @@ const SidebarMenuAction = React.forwardRef<
           'group-focus-within/menu-item:opacity-100 group-hover/menu-item:opacity-100 data-[state=open]:opacity-100 peer-data-[active=true]/menu-button:text-sidebar-accent-foreground md:opacity-0',
         className,
       )}
-      {...props}
+      {...(props as any)}
     />
   )
 })
@@ -737,7 +737,7 @@ const SidebarMenuSubButton = React.forwardRef<
         'group-data-[collapsible=icon]:hidden',
         className,
       )}
-      {...props}
+      {...(props as any)}
     />
   )
 })
